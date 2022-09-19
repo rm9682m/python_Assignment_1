3ddc0eb190c042bb8a7c6c060e4a86481eea9446
rm9682m <113205667+rm9682m@users.noreply.github.com>
2022-09-19 12:07:56 -0700

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,48 @@
+# 1. Write a command to get the Python version you are using.
+# import sys
+# print(sys.version)
+
+
+# 2. Write a python script to print Hello Python on the screen directly in the command line.
+# print("Hello Python")
+
+
+# 3. Write a python script and store it in a file and execute the file from the command line.
+# import A1
+# print(A1.a)
+
+
+# 4. Write a python script to print Hello Python on the screen using visual studio code.
+# print("Hello Python")
+
+
+# 5. Write a python script to print Hello on the first line and Python on the second line.
+# print('Hello','Python',sep='\n')
+
+
+# 6. Write a python script to print “MySirG” on the screen.
+# print('"My''SirG"')
+
+
+# 7. Write a python script to print “Teacher’s Day” on the screen.
+# print('"Teacher','s Day"',sep="'")
+
+
+# 8. Write a python script to print the value of a variable. Variable contains your name as data.
+# n="Rahul Mishra"
+# print(n)
+
+
+# 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+# name="Rahul Mishra"
+# age=20
+# qualification="B.tech"
+# yearsofexperience=2
+# print(name,age,qualification,yearsofexperience,sep='\n')
+
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable contains integer values.
+# v=2
+# r=4
+# t=6
+# print(v,r,t,sep='\n')
